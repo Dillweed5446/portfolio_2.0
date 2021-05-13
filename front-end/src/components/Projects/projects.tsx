5262a040d8879a1ac923f7ebb47c2d006cfe76e2
Paul Dill <paul@pdill.dev>
2021-05-13 13:15:00 -1000

    :art: :construction: :lipstick: Finished tech icons setup.  Only need some minor style tweeks (maybe) and the project images.

--- front-end/src/components/Projects/projects.tsx
+++ front-end/src/components/Projects/projects.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { FaCode } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
 import { GoBrowser } from 'react-icons/go'
 import { SectionTitle } from '../../styles/globalStyledComponents'
 import StyledIcons from './styledIcon'
@@ -24,19 +24,12 @@ background-color: green;
 margin: auto;
 `
 
-const CardIcons = styled.div`
-width: 90%;
-height: 3rem;
-background-color: transparent;
-margin: auto;
-margin-top: .5rem;
-font-size: 2.5rem;
-`
 const CardButton = styled.button`
 color: #ffc857ff;
 border-bottom: 2px solid #4ea5d9ff;
+text-shadow: -1px 0 #14080eff, 0 1px black, 1px 0 #14080eff, 0 -1px black;
 padding: 2rem;
-margin: .5rem;
+margin: 0 .5rem;
 background: #8acb88ff;
 font-size: 2rem;
 font-weight: 800;
@@ -44,54 +37,6 @@ border-radius: .7rem;
 `
 
 export default function Projects ({ className, style, projectArray }: Props) {
-  // Create map function with array for projects so that I can add github link and auto populate the cards.
-  // May try borrowing namedIcons module from Attila Goz to simplify this section
-
-  /* <Card style={{ gridRow: 'title-end', gridColumn: 'line2' }}>
-        <CardTitle>this.portfolio</CardTitle>
-        <CardImage>Insert Image Here.</CardImage>
-        <CardIcons><FaReact/></CardIcons>
-        <div style={{ textAlign: 'center', marginTop: '1rem' }}>
-          <a href="/#top" ><CardButton><GoBrowser/>  Demo</CardButton></a>
-          <a href="https://github.com/Dillweed5446/portfolio_2.0"><CardButton><FaCode/>  Code</CardButton></a>
-        </div>
-      </Card>
-      <Card style={{ gridRow: 'title-end', gridColumn: 'line3' }}>
-        <CardTitle>Hui Wa&apos;a Weather</CardTitle>
-        <CardImage>Insert Image Here.</CardImage>
-        <CardIcons><FaReact/></CardIcons>
-        <div style={{ textAlign: 'center', marginTop: '1rem' }}>
-          <a href="https://pdill.dev/paddleWrapper" ><CardButton><GoBrowser/>  Demo</CardButton></a>
-          <a href="https://github.com/Dillweed5446/pdill.dev/tree/master/pdill.dev/components/Paddler_App" ><CardButton><FaCode/>  Code</CardButton></a>
-        </div>
-      </Card>
-      <Card style={{ gridRow: 'first-card-end', gridColumn: 'line2' }}>
-        <CardTitle>Library CRUD</CardTitle>
-        <CardImage>Insert Image Here.</CardImage>
-        <CardIcons><FaReact/></CardIcons>
-        <div style={{ textAlign: 'center', marginTop: '1rem' }}>
-          <a href="" ><CardButton><GoBrowser/>  Demo</CardButton></a>
-          <a href="https://github.com/Dillweed5446/library_crud" ><CardButton><FaCode/>  Code</CardButton></a>
-        </div>
-      </Card>
-      <Card style={{ gridRow: 'first-card-end', gridColumn: 'line3' }}>
-        <CardTitle>MotherCupboard</CardTitle>
-        <CardImage>Insert Image Here.</CardImage>
-        <CardIcons><FaReact/></CardIcons>
-        <div style={{ textAlign: 'center', marginTop: '1rem' }}>
-          <a href="https://mothercupboard.com" ><CardButton><GoBrowser/>  Demo</CardButton></a>
-          <a href="https://github.com/aptlyundecided/mc-frontend" ><CardButton><FaCode/>  Code</CardButton></a>
-        </div>
-      </Card>
-      <Card style={{ gridRow: 'second-card-end', gridColumn: 'line2' }}>
-        <CardTitle>Next.js Portfolio</CardTitle>
-        <CardImage>Insert Image Here.</CardImage>
-        <CardIcons><FaReact/></CardIcons>
-        <div style={{ textAlign: 'center', marginTop: '1rem' }}>
-          <a href="https://pdill.dev" ><CardButton><GoBrowser/>  Demo</CardButton></a>
-          <a href="https://github.com/Dillweed5446/pdill.dev" ><CardButton><FaCode/>  Code</CardButton></a>
-        </div>
-      </Card> */
   return (
     <div style={{ padding: '2rem', backgroundColor: '#4ea5d9ff' }}>
       <header>
@@ -104,10 +49,10 @@ export default function Projects ({ className, style, projectArray }: Props) {
           <div className="card" key={`card-${index + 1}`}>
             <CardTitle>{item.title}</CardTitle>
             <CardImage>{item.image}</CardImage>
-            <CardIcons><StyledIcons iconArray={item.icons} /></CardIcons>
+            <StyledIcons iconArray={item.icons}/>
             <div style={{ textAlign: 'center', marginTop: '1rem' }}>
             <a href={item.demoHref}><CardButton><GoBrowser/>  Demo</CardButton></a>
-            <a href={item.codeHref}><CardButton><FaCode/>  Code</CardButton></a>
+            <a href={item.codeHref}><CardButton><FaGithub/>  Code</CardButton></a>
             </div>
           </div>
           )
--- front-end/src/components/Projects/styledIcon.tsx
+++ front-end/src/components/Projects/styledIcon.tsx
@@ -8,32 +8,37 @@ interface Props {
 }
 
 const IconWrapper = styled.div`
+max-width: 100%;
+display: flex;
+flex-direction: row;
+justify-content: flex-start;
+align-items: center;
+margin-bottom: 1rem;
+padding: 0 2rem;
+`
+
+const CardIcons = styled.div`
+flex: 1;
 display: flex;
 flex-direction: column;
-justify-content: space-between;
 align-items: center;
+// justify-content: center;
+padding: 1rem 0;
+height: 3rem;
+background-color: transparent;
 `
 
 export default function StyledIcon ({ style, iconArray, children }:Props) {
   return (
-      <div>
+            <IconWrapper >
       {iconArray.map((item:any, index:number) => {
         return (
-              <IconWrapper key={index}>
-                <div>
-                    {item.map((item:any, index:number) => {
-                      return (
-                            <div key={index}>
-                                {item.icon}
-                                {item.tech}
-                            </div>
-                      )
-                    }
-                    )}
-                </div>
-                    </IconWrapper>
+                <CardIcons key={index}>
+                    <img style={{ height: '2rem' }}src={item.icon} alt={item.tech}/>
+                    <p style={{ fontSize: '1rem', margin: '0', textAlign: 'center' }}>{item.tech}</p>
+                </CardIcons>
         )
       })}
-        </div>
+    </IconWrapper>
   )
 }
